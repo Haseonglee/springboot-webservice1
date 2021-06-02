0b92954ef3a370d7d0dea35dd55791f5a7cda622
Haseonglee <hhh4389@naver.com>
2021-06-02 17:03:58 +0900

    index 페이지

--- src/main/java/com/seong/springboot/Application.java
+++ src/main/java/com/seong/springboot/Application.java
@@ -2,7 +2,10 @@ package com.seong.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
